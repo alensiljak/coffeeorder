bd88da635c886c70d7bab4ca37ee955ba96643cc
Alen Siljak <alen.siljak@gmail.com>
2011-10-05 13:31:44 +0200

    Adding hello message.

--- CoffeeOrder/Controllers/HomeController.cs
+++ CoffeeOrder/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace CoffeeOrder.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to Coffee Order site!";
 
             return View();
         }
